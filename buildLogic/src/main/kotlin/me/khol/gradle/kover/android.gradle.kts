36b7a84b64abfe5eba8a00aeb01a1efafb137ce1
David Khol <david.khol@bucketplace.net>
2024-06-16 12:02:34 +0900

    Resolve API changes

--- buildLogic/src/main/kotlin/me/khol/gradle/kover/android.gradle.kts
+++ buildLogic/src/main/kotlin/me/khol/gradle/kover/android.gradle.kts
@@ -4,12 +4,9 @@ plugins {
     id("me.khol.gradle.kover.base")
 }
 
-koverReport {
-    defaults {
-        // Tasks such as `koverXmlReport` will automatically include coverage for the debug variant.
-        // If not configured, `koverXmlReport` will produce empty coverage.
-        // We can still run `koverXmlReportDebug`, but then it is a bit more complicated to
-        // configure from the root build.gradle.kts script.
-        mergeWith("debug")
+kover {
+    reports {
+        total {
+        }
     }
 }
